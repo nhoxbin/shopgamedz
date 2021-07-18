e396d623eb765367b0b8294547a2bba15e10a7ae
Anh Khoa <binm8.4356@gmail.com>
2021-07-18 22:13:18 +0700

    Fix lỗi

--- resources/views/box-event-instruction.blade.php
+++ resources/views/box-event-instruction.blade.php
@@ -7,12 +7,16 @@
             <div class="col-xs-12">
                 <div class="x_panel">
                     <div class="x_content">
-                        Kết Quả Mã Hóa MD5 làm minh bạch giải thưởng ! Kết quả không thể thay đổi ! Sau khi kết quả được công cố sẽ hiện thị kết quả chưa mã hóa ! Bạn coppy kết quả này bỏ vào công cụ mã hóa MD5 bất kì trên google sau đó mã hóa và so sánh với Kết Quả Mã Hóa MD5 ban đầu sẽ giống nhau 100%
-Bạn lo lắng ai đó gian lận bằng cách hack MD5 ? Yên tâm trên thế giới này chưa ai hack được !
-1 Số Trang mã hóa MD5 cho mọi người check :
-https://www.md5hashgenerator.com/
-https://www.webatic.com/md5-convertor
-https://bietmaytinh.com/md5/
+                        <div>
+                            Kết Quả Mã Hóa MD5 làm minh bạch giải thưởng ! Kết quả không thể thay đổi ! Sau khi kết quả được công cố sẽ hiện thị kết quả chưa mã hóa ! Bạn coppy kết quả này bỏ vào công cụ mã hóa MD5 bất kì trên google sau đó mã hóa và so sánh với Kết Quả Mã Hóa MD5 ban đầu sẽ giống nhau 100%
+                        </div>
+                        <div>
+                            Bạn lo lắng ai đó gian lận bằng cách hack MD5 ? Yên tâm trên thế giới này chưa ai hack được !
+                        </div>
+                        <div>1 Số Trang mã hóa MD5 cho mọi người check:</div>
+                        <div><a href="https://www.md5hashgenerator.com/" target="_blank">https://www.md5hashgenerator.com/</a></div>
+                        <div><a href="https://www.webatic.com/md5-convertor" target="_blank">https://www.webatic.com/md5-convertor</a></div>
+                        <div><a href="https://bietmaytinh.com/md5/" target="_blank">https://bietmaytinh.com/md5/</a></div>
                     </div>
                 </div>
             </div>
